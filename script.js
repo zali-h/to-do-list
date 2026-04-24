3f7f7f20958182def913f34438cad88e063882d5
zali-h <213874294+zali-h@users.noreply.github.com>
2026-04-25 00:30:36 +0800

    Set constants and start initialising array in .js for todo app

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Retrieve todo from local storage or initialise an empty array
+
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
+// Initialise
+document.addEventListener("DOMContentLoaded", 
+    function() {
+        addButton.addEventListener("click", addTask();
+        todoInput.addEventListener("keydown", function(event) {
+            if (event.key == "Enter") {
+                event.preventDefault();
+                addTask();
+            }
+        })
+    }
+)
+
+function addTask() {
+
+}
